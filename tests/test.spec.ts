0e1f8ded4b9973153ca692221dd7dffc9ccb6fd6
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2025-11-03 12:16:17 +0530

    created this repo

---
+++ tests/test.spec.ts
@@ -0,0 +1,11 @@
+
+import { test } from "@playwright/test";
+import { click, launchURL, findElementUniversal } from "@tx/playwright-core";
+
+test("login", async ({ page }) => {
+    await launchURL(page, "https://google.com");
+    const searchBox = await findElementUniversal(
+        page, ['input[name="3q1"]', '[jsname="yZiJb0e"]', '[class="gLFyf"]'], "Search Box"
+    );
+    await click(page, searchBox);
+});
