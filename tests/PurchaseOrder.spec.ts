7e09112345446decd166954790ff219647a8c169
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2025-11-14 13:21:56 +0530

    purchase order

--- tests/PurchaseOrder.spec.ts
+++ tests/PurchaseOrder.spec.ts
@@ -5,7 +5,8 @@ import { click, launchURL, findElementUniversal } from "@tx/playwright-core";
 test("Purchase Order", async ({ page }) => {
     await launchURL(page, '/');
     const searchBox = await findElementUniversal(
-        page, ['input[name="3q1"]', '[jsname="yZiJb0e"]', '[class="gLFyf"]'], "Search Box"
+        page, ['input[name="3q1"]', '[jsname="yZiJb0e"]',
+        '[class="gLFyf"]'], "Search Box"
     );
     await click(page, searchBox);
 });
